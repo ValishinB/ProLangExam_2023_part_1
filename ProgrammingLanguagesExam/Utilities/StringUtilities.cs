f75b7585654ab2d275ea1778cfa6769ff475e00b
ValishinB <125293798+ValishinB@users.noreply.github.com>
2023-06-17 05:11:06 +0200

    Update StringUtilities.cs

--- ProgrammingLanguagesExam/Utilities/StringUtilities.cs
+++ ProgrammingLanguagesExam/Utilities/StringUtilities.cs
@@ -2,5 +2,19 @@
 
 public class StringUtilities
 {
+public static int ToWords(string input)
+    {
+        string[] words = input.Split(' ');
+        HashSet<string> uniqueWords = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
 
+        foreach (string word in words)
+        {
+            if (!string.IsNullOrWhiteSpace(word))
+            {
+                uniqueWords.Add(word);
+            }
+        }
+
+        return uniqueWords.Count;
+    }
 }
